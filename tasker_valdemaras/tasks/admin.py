e0cecccc83d9d49da25a8f39c0249d6222842d1d
vokator <vokator@gmail.com>
2024-02-13 10:10:18 +0200

    Tasker failų koregavimas

--- tasker_valdemaras/tasks/admin.py
+++ tasker_valdemaras/tasks/admin.py
@@ -4,13 +4,13 @@ from . import models
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'total_tasks', 'undone_tasks', 'owner', 'recent_tasks']
+    list_display = ['name', 'total_tasks', 'undone_tasks', 'owner', 'recent_tasks']
     list_display_links = ['name']
     list_filter = ['owner']
     search_fields = ['name']
     fieldsets = (
         (None, {
-            'fields': (
+            "fields": (
                 'name', 'owner',
             ),
         }),
@@ -18,37 +18,36 @@ class ProjectAdmin(admin.ModelAdmin):
 
     def total_tasks(self, obj: models.Project):
         return obj.tasks.count()
-    total_tasks.short_description = _("number of tasks")
+    total_tasks.short_description = _("total tasks")
 
     def undone_tasks(self, obj: models.Project):
         return obj.tasks.filter(is_done=False).count()
     undone_tasks.short_description = _("undone tasks")
 
     def recent_tasks(self, obj: models.Project):
-        return "; ".join(obj.tasks.order_by('-created_at').values_list('name'))
-
-
+        return "; ".join(obj.tasks.order_by('-created_at').values_list('name', flat=True)[:3])
+    recent_tasks.short_description = _("recent tasks")
 
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['name', 'is_done', 'deadline', 'project', 'owner', 'created_at']
-    list_filter = ['is_done', 'project', 'owner', 'deadline', 'created_at']
-    search_fields = ['name', 'description', 'project__name', 'owner__last_name', 'owner__user_name']
+    list_filter = ['is_done', 'deadline', 'created_at']
+    search_fields = ['name', 'description', 'project__name', 'owner__last_name', 'owner__username']
     list_editable = ['is_done', 'owner', 'project']
     readonly_fields = ['id', 'created_at', 'updated_at']
     fieldsets = (
-        (_('general').title(), {
+        (_("general").title(), {
             "fields": (
                 ('name', 'deadline'),
                 'description', 'is_done',
             ),
         }),
-        (_('ownership').title, {
+        (_("ownership").title(), {
             "fields": (
                 ('owner', 'project'),
             ),
         }),
-        (_('temporal tracking').title, {
+        (_("temporal tracking").title(), {
             "fields": (
                 ('created_at', 'updated_at', 'id'),
             ),
--- tasker_valdemaras/tasks/models.py
+++ tasker_valdemaras/tasks/models.py
@@ -8,18 +8,21 @@ class Project(models.Model):
     name = models.CharField(_("name"), max_length=100, db_index=True)
     owner = models.ForeignKey(
         get_user_model(), 
-        on_delete=models.CASCADE, 
         verbose_name=_("owner"), 
+        on_delete=models.CASCADE,
         related_name='projects',
     )
-
-    def __str__(self):
-        return self.name
 
     class Meta:
         verbose_name = _("project")
         verbose_name_plural = _("projects")
         ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("project_detail", kwargs={"pk": self.pk})
 
 
 class Task(models.Model):
@@ -27,15 +30,15 @@ class Task(models.Model):
     description = models.TextField(_("description"), blank=True, max_length=10000)
     project = models.ForeignKey(
         Project,
-        on_delete=models.CASCADE, 
         verbose_name=_("project"), 
+        on_delete=models.CASCADE,
         related_name='tasks',
     )
     owner = models.ForeignKey(
         get_user_model(), 
-        on_delete=models.CASCADE, 
         verbose_name=_("owner"), 
-        related_name='tasks',
+        on_delete=models.CASCADE,
+        related_name='tasks'
     )
     created_at = models.DateTimeField(_("created at"), auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(_("updated at"), auto_now=True, db_index=True)
@@ -43,12 +46,12 @@ class Task(models.Model):
     deadline = models.DateTimeField(_("deadline"), null=True, blank=True, db_index=True)
 
     class Meta:
-            verbose_name = _("task")
-            verbose_name_plural = _("tasks")
-            ordering = ['is_done', '-created_at']
+        verbose_name = _("task")
+        verbose_name_plural = _("tasks")
+        ordering = ['is_done', '-created_at']
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-         return reverse("task_detail", kwargs={"pk": self.pk})
+        return reverse("task_detail", kwargs={"pk": self.pk})
