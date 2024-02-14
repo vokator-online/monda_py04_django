d663755aa7b43c286c5fdb2aae8529a1fe6eca8c
Kestutis Januskevicius <infohata@gmail.com>
2024-02-14 10:03:18 +0200

    PTU20 task mark done done done done

--- tasker_ptu20/tasks/urls.py
+++ tasker_ptu20/tasks/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('tasks/', views.task_list, name='task_list'),
     path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/<int:pk>/done/', views.task_done, name='task_done'),
 ]
