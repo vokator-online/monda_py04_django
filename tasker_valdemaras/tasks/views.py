756494c8a30c5933889e75199eb468dc25089ba5
vokator <vokator@gmail.com>
2024-02-14 10:35:33 +0200

    Urls, Views, Base, Task atnaujinimas

--- tasker_valdemaras/tasks/views.py
+++ tasker_valdemaras/tasks/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from . import models
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -21,7 +21,11 @@ def task_detail(request: HttpRequest, pk: int) -> HttpResponse:
         'task': get_object_or_404(models.Task, pk=pk),
     })
 
-def task_done(...):
-    # ...
-    messages.success(request, f"Task #{task.pk} marked as {'done' if task.is_done else 'undone'}.")
+def task_done(request: HttpRequest, pk: int) -> HttpResponse:
+    task = get_object_or_404(models.Task, pk=pk)
+    task.is_done = not task.is_done
+    task.save()
+    messages.success(request, f"Task {task.name} marked as {'done' if task.is_done else 'undone'}.")
+    if request.GET.get('next'):
+        return redirect(request.GET.get('next'))
     return redirect(task_list)
