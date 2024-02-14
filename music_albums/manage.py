0331141ad482e81ffc0757ca13a91fea7d5f515e
vokator <vokator@gmail.com>
2024-02-14 13:42:48 +0200

    Medžiagos atnaujinimas

--- music_albums/manage.py
+++ music_albums/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_album.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_albums.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- music_albums/music_albums/urls.py
+++ music_albums/music_albums/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for music_album project.
+URL configuration for music_albums project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard/', views.Home.as_view(), name='home'),
+    path('grappelli/', include('grappelli.urls')),
 ]
