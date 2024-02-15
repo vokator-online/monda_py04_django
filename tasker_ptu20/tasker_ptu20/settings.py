d053a5854ee8ed4616e1d1d5a30504bf10798b9a
Kestutis Januskevicius <infohata@gmail.com>
2024-02-15 14:21:32 +0200

    PTU20 language picker + italiano

--- tasker_ptu20/tasker_ptu20/settings.py
+++ tasker_ptu20/tasker_ptu20/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -104,12 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'lt'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'en-us'
+LANGUAGES = [
+    (LANGUAGE_CODE, 'US English'),
+    ('lt', 'Lietuvių'),
+    ('it', 'Italiano'),
+]
+TIME_ZONE = 'Europe/Vilnius'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
