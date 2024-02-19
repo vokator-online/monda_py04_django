53fea2f02ecd0d6d05f0168f3dcd19f34112fbed
Kestutis Januskevicius <infohata@gmail.com>
2024-02-19 10:05:37 +0200

    PTU20 user menu and profile detail view

--- tasker_ptu20/user_profile/urls.py
+++ tasker_ptu20/user_profile/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('detail/', views.user_detail, name='user_detail_current'),
+    path('detail/<str:username>/', views.user_detail, name='user_detail'),
 ]
