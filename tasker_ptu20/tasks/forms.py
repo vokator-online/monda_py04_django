220732d05fc9ba2bf75780aff5617c8aebe7c187
Kestutis Januskevicius <infohata@gmail.com>
2024-02-22 09:02:49 +0200

    pumped up dashboard PTU20

--- tasker_ptu20/tasks/forms.py
+++ tasker_ptu20/tasks/forms.py
@@ -2,7 +2,14 @@ from django import forms
 from . import models
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = models.Task
         fields = ('name', 'project', 'description', 'deadline', 'is_done', )
+        widgets = {
+            'deadline': DateInput,
+        }
